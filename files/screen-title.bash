6508828397d6b627f2dbf5aefebba4a680ccf16d
SleJP <j.p.g.sleddens@hr.nl>
2020-09-05 11:24:33 +0200

    update

---
+++ files/screen-title.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+array=(${PWD//\// })
+HN=$( echo $HOSTNAME | tr [:lower:] [:upper:] )
+
+TITLE="$HN:./${array[-1]}"
+
+if [[ "$PWD" =~ $HOME/src/(.*) ]] ; then
+  if [ "${array[3]}" == "src" ] && [ ! -z "${array[4]}" ]; then
+    TITLE="G:${array[4]}"
+  fi
+fi
+
+if [ "$PWD" == "$HOME" ]; then
+  TITLE="$HN:~"
+fi
+
+printf "\033k%s\033\\" "$TITLE"
